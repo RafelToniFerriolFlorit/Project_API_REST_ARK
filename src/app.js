fc9c7d98a3a748c634ac1fe37b8be377685473e3
RafelToniFerriolFlorit <rafeltoniferriol@paucasesnovescifp.cat>
2026-02-16 15:35:49 +0100

    Branch task4 Terminada

--- src/app.js
+++ src/app.js
@@ -1,9 +1,18 @@
 var express = require('express');
 var app = express();
 
+
+app.use(express.json());
+app.set("trust proxy", 1);
+
 const criaturesRoutes = require ('./routes/dinos-routes.js');
 app.use ('/api/ark', criaturesRoutes);
 
-app.use (express.json());
-app.listen (3000, () => console.log ('Servidor furula en http://localhost:3000'));
+
+const PORT = process.env.PORT || 3000;
+
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Servidor corriendo en http://0.0.0.0:${PORT}`);
+});
 
